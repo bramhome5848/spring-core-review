f82d879fd1bacb70542b086d95780cec49712792
KyungjinLee <bramhome5848@gmail.com>
2023-02-25 17:37:56 +0900

    20230225 프로젝트 생성 및 빌드

---
+++ src/main/java/hello/springcorereview/SpringCoreReviewApplication.java
@@ -0,0 +1,13 @@
+package hello.springcorereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/springcorereview/SpringCoreReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcorereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
