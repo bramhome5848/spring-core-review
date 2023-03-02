70824d70f5cd1533b49146a4a95da52340b528e1
KyungjinLee <bramhome5848@gmail.com>
2023-03-02 23:32:25 +0900

    20230302 스프링 컨테이너와 스프링 빈 1

---
+++ src/test/java/hello/springcorereview/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,44 @@
+package hello.springcorereview.beanfind;
+
+import hello.springcorereview.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    /**
+     * ApplicationContext -> 스프링 컨테이너라 한다.
+     * ApplicationContext -> 인터페이스이다.
+     * new AnnotationConfigApplicationContext(AppConfig.class) -> 스프링 컨테이너를 생성할 떄는 구성 정보를 지정해주어야 한다.
+     * AppConfig.class 를 구성정보로 지정했다.
+     */
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE : 스프링 내우에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
